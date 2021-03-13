2ac84596be871105b0ba5379bad7b806758a1fdd
CentRa <68494279+CentRa-Linux@users.noreply.github.com>
2021-03-13 21:03:32 +0900

    Update screenshots.tsx

--- src/pages/screenshots.tsx
+++ src/pages/screenshots.tsx
@@ -13,7 +13,7 @@ export default function Index() {
         <h2>
           「軽くて早くて即使える」32BitDebianカスタムイメージ
         </h2>
-        <div class="wrap">
+        <div className="wrap">
           <iframe src="https://albumizr.com/a/l9dh" scrolling="no" frameBorder="0" allowFullScreen width="700" height="400"></iframe>
         </div>
       </div>
