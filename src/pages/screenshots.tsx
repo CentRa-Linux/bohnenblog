2c6a6a1a453272cc6d3943eeb3547586830562a8
CentRa <68494279+CentRa-Linux@users.noreply.github.com>
2021-03-13 20:46:02 +0900

    Create screenshots.tsx

---
+++ src/pages/screenshots.tsx
@@ -0,0 +1,20 @@
+
+import Header from '../components/header'
+import ExtLink from '../components/ext-link'
+import Features from '../components/features'
+import sharedStyles from '../styles/shared.module.css'
+
+export default function Index() {
+  return (
+    <>
+      <Header titlePre="Home" />
+      <div className={sharedStyles.layout}>
+        <h1>スクリーンショット</h1>
+        <h2>
+          「軽くて早くて即使える」32BitDebianカスタムイメージ
+        </h2>
+        <iframe src="https://albumizr.com/a/l9dh" scrolling="no" frameborder="0" allowfullscreen width="700" height="400"></iframe>
+      </div>
+    </>
+  )
+}
