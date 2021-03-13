9763f4274d786531ad9fe6556a990a376098f523
CentRa <68494279+CentRa-Linux@users.noreply.github.com>
2021-03-13 20:33:10 +0900

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,21 +4,14 @@ export default function Footer() {
   return (
     <>
       <footer>
-        <span>Deploy your own!</span>
-        <ExtLink href="https://vercel.com/new/git/external?repository-url=https://github.com/ijjk/notion-blog/tree/main&project-name=notion-blog&repository-name=notion-blog">
+        <span>Download!</span>
+        <ExtLink href="https://osdn.net/projects/bohnenlinux/downloads/74761/bohnenlinux-i386-2021-r1.iso/">
           <img
-            src="https://vercel.com/button"
+            src="https://github.com/CentRa-Linux/bohnenblog/raw/master/are.svg"
             height={46}
             width={132}
             alt="deploy to Vercel button"
           />
-        </ExtLink>
-        <span>
-          or{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog">
-            view source
-          </ExtLink>
-        </span>
       </footer>
     </>
   )
